91c2a5001e30595de617383ae5a0ee92ba2c9954
BenCheng2 <bencheng0609@gmail.com>
2024-03-27 17:03:29 -0400

    feat: Prediction Button on SingleButtonGUI
    Add a prediction button on SingleButtonGUI.

--- GUI/SingleButtonGUI.py
+++ GUI/SingleButtonGUI.py
@@ -1,9 +1,22 @@
 import tkinter as tk
-from PositionRetrieve.Redis import store_network_info
 
-def on_button_click():
+from Position.Redis import store_network_info, get_network_info
+from Prediction.KNN_Predict import predict
+from Prediction.LoadFromRedis import load_from_redis_into_X_y, load_from_redis_all_bssid
+
+
+def on_record_button_click(): # Record the position information
     area_name = entry.get()
     store_network_info(area_name)
+
+
+def on_predict_button_click(): # Predict the current position
+    bssids = load_from_redis_all_bssid()
+    row = get_network_info(bssids)
+    X, y = load_from_redis_into_X_y(bssids)
+    result = predict(X, y, row)
+    print(result)
+
 
 root = tk.Tk()
 root.title("Simple GUI")
@@ -13,11 +26,10 @@ root.geometry("300x100")
 entry = tk.Entry(root)
 entry.pack(pady=10)
 
-button = tk.Button(root, text="Click", command=on_button_click)
-
-predict_button = tk.Button(root, text="Predict")
-
+button = tk.Button(root, text="Click", command=on_record_button_click)
 button.pack(expand=True)
+
+predict_button = tk.Button(root, text="Predict", command=on_predict_button_click)
 predict_button.pack(expand=True)
 
 root.mainloop()
