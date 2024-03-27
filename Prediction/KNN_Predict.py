050b30248d171243c57d20d91500faf2755fe5ab
BenCheng2 <bencheng0609@gmail.com>
2024-03-27 17:08:34 -0400

    feat: Formal KNN prediction function
    Add a KNN prediction function that predict updating the model when no
    changes to the dataset is made.

---
+++ Prediction/KNN_Predict.py
@@ -0,0 +1,19 @@
+from GlobalVariable import KNN_Model
+
+PAST_LENGTH = 0
+
+
+def predict(X, y, current):
+    """
+    Predict the current area using the KNN model
+    :param current:
+    :return:
+    """
+    global PAST_LENGTH
+
+    if len(X) != PAST_LENGTH:
+        KNN_Model.fit(X, y)
+        PAST_LENGTH = len(X)
+
+    result = KNN_Model.predict([current])
+    return result[0]
