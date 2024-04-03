0c3aaa42ecafffe2eb66500fdee2489a10928606
BenCheng2 <bencheng0609@gmail.com>
2024-04-03 12:19:23 -0400

    feat: Saving GPT start conversation
    Saving the start conversation to the env file

--- GPT.py
+++ GPT.py
@@ -17,29 +17,8 @@ model_version = os.getenv('OPENAI_MODEL_VERSION')
 local_messages = []
 
 # Start message
-system_msg = "You are a Cartographer bot.\
-You will introduce yourself as a Cartographer whose role is to ask the user some questions about their visit.\
-Your role is to ask the user the following questions in a professional manner, one question at a time.\
-You should skip questions when the user has already provided an answer to a previous question you asked. \
-You should follow up on questions whenever the response given by the user is vague.\
-You should collect information about a building to develop a spatial understanding of the layout.\
-The user might tell you about multiple connected spaces. You should ask about the next spatial area, one space at a time.\
-You should continue this process until the user feels like they described all that they want to tell you.\
-You should not ask the user to choose the next area, you should choose by yourself.\
-You must use a list to display and store explored areas.\
-Here are the questions to ask:\
-1. What room do you want to start with?\
-3. What are other spaces near to [XXX]?\
-4. Where [XXX] is located in relative to [XXX]?\
-5. What is the short description of area [XXX]?\
-[XXX] only represent spaces\
-Do not ask 2 or more questions each time.\
-Your current description explored area list is [].\
-Your current near space known area list is [].\
-Your current unexplored area list is [].\
-An area is unexplored if its description and areas near it not found. \
-You must display this list in each time you talk and update it if possible.\
-"
+system_msg = os.getenv('SYSTEM_MSG')
+
 
 
 def start_conversation():
