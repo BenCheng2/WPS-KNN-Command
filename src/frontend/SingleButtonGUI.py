10c4936ec04bdc8d2a3c7fd188dab5feea012c1d
BenCheng2 <bencheng0609@gmail.com>
2024-06-03 15:17:10 -0400

    feat: Rebuild Single Button GUI

--- src/frontend/SingleButtonGUI.py
+++ src/frontend/SingleButtonGUI.py
@@ -1,11 +1,10 @@
 import tkinter as tk
-import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-
 
 from src.backend.KNN_Predict import predict_knn
 
 from src.database.RedisClass import RedisClass
+
 RedisClass = RedisClass()
 load_from_redis_all_bssid = RedisClass.load_from_redis_all_bssid
 load_into_X_y = RedisClass.load_into_X_y
@@ -13,10 +12,10 @@ load_from_redis_all_names_and_data = RedisClass.load_from_redis_all_names_and_da
 store_network_info = RedisClass.store_network_info
 get_network_info = RedisClass.get_network_info
 
+
 def on_record_button_click():  # Record the position information
     area_name = entry.get()
     store_network_info(area_name)
-
 
 
 def on_predict_button_click():  # Predict the current position
@@ -31,20 +30,6 @@ def on_predict_button_click():  # Predict the current position
 
 load_from_redis_all_names_and_data()
 
-#
-# root.geometry("300x100")
-#
-# entry = tk.Entry(root)
-# entry.pack(pady=10)
-#
-# button = tk.Button(root, text="Click", command=on_record_button_click)
-# button.pack(expand=True)
-#
-# predict_button = tk.Button(root, text="Predict", command=on_predict_button_click)
-# predict_button.pack(expand=True)
-#
-# root.mainloop()
-
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Simple Button GUI")
@@ -56,18 +41,13 @@ if __name__ == '__main__':
     entry = tk.Entry(root, width=30)
     entry.pack(padx=5, ipady=10, expand=True)
 
-    # Frame for buttons to help in alignment
     button_frame = tk.Frame(root)
     button_frame.pack(expand=True, fill=tk.X)
 
-    # Button for 'Click'
-    button = tk.Button(button_frame, text="Click",  width=16, command=on_record_button_click)
+    button = tk.Button(button_frame, text="Click", width=16, command=on_record_button_click)
     button.pack(side=LEFT, padx=5, expand=True)
 
-    # Button for 'Predict'
     predict_button = tk.Button(button_frame, text="Predict", width=16, command=on_predict_button_click)
     predict_button.pack(side=RIGHT, padx=5, expand=True)
 
-
     root.mainloop()
-
