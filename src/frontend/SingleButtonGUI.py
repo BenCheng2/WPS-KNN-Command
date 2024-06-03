101fbd658ea2dd05f81300611d8f62d781c9d088
BenCheng2 <bencheng0609@gmail.com>
2024-06-03 15:16:42 -0400

    feat: Rebuild Single Button GUI

--- src/frontend/SingleButtonGUI.py
+++ src/frontend/SingleButtonGUI.py
@@ -1,20 +1,22 @@
 import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
-from src.database.GlobalVariable import get_all_data
-from src.database.Redis import store_network_info, get_network_info
+
 from src.backend.KNN_Predict import predict_knn
 
-from src.database.Redis_class import Redis_class
-
-load_from_redis_all_bssid = Redis_class.load_from_redis_all_bssid
-load_into_X_y = Redis_class.load_into_X_y
-load_from_redis_all_names_and_data = Redis_class.load_from_redis_all_names_and_data
+from src.database.RedisClass import RedisClass
+RedisClass = RedisClass()
+load_from_redis_all_bssid = RedisClass.load_from_redis_all_bssid
+load_into_X_y = RedisClass.load_into_X_y
+load_from_redis_all_names_and_data = RedisClass.load_from_redis_all_names_and_data
+store_network_info = RedisClass.store_network_info
+get_network_info = RedisClass.get_network_info
 
 def on_record_button_click():  # Record the position information
     area_name = entry.get()
     store_network_info(area_name)
 
-    print(get_all_data())
 
 
 def on_predict_button_click():  # Predict the current position
@@ -28,20 +30,44 @@ def on_predict_button_click():  # Predict the current position
 
 
 load_from_redis_all_names_and_data()
-print(get_all_data())
 
-root = tk.Tk()
-root.title("Simple GUI")
+#
+# root.geometry("300x100")
+#
+# entry = tk.Entry(root)
+# entry.pack(pady=10)
+#
+# button = tk.Button(root, text="Click", command=on_record_button_click)
+# button.pack(expand=True)
+#
+# predict_button = tk.Button(root, text="Predict", command=on_predict_button_click)
+# predict_button.pack(expand=True)
+#
+# root.mainloop()
 
-root.geometry("300x100")
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Simple Button GUI")
 
-entry = tk.Entry(root)
-entry.pack(pady=10)
+    root.geometry("400x160")
+    default_font = ('Arial', 16)
+    root.option_add("*Font", default_font)
 
-button = tk.Button(root, text="Click", command=on_record_button_click)
-button.pack(expand=True)
+    entry = tk.Entry(root, width=30)
+    entry.pack(padx=5, ipady=10, expand=True)
 
-predict_button = tk.Button(root, text="Predict", command=on_predict_button_click)
-predict_button.pack(expand=True)
+    # Frame for buttons to help in alignment
+    button_frame = tk.Frame(root)
+    button_frame.pack(expand=True, fill=tk.X)
 
-root.mainloop()
+    # Button for 'Click'
+    button = tk.Button(button_frame, text="Click",  width=16, command=on_record_button_click)
+    button.pack(side=LEFT, padx=5, expand=True)
+
+    # Button for 'Predict'
+    predict_button = tk.Button(button_frame, text="Predict", width=16, command=on_predict_button_click)
+    predict_button.pack(side=RIGHT, padx=5, expand=True)
+
+
+    root.mainloop()
+
